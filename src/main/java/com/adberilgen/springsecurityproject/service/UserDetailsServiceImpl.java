5ed731e0be1826ca636c443ff28d17ef715664e9
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-11-23 16:50:45 +0300

    Base Auth || UserDetailsServiceImpl added.

---
+++ src/main/java/com/adberilgen/springsecurityproject/service/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.adberilgen.springsecurityproject.service;
+
+import com.adberilgen.springsecurityproject.model.User;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userService.getByUserName(username);
+        return user.orElseThrow((EntityNotFoundException::new));
+    }
+}
