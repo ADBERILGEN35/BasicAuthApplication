953427a64732ad79524090bca88aa0fca13fb55a
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-11-23 16:49:53 +0300

    Base Auth || CreateUserRequest and Role updated.

--- src/main/java/com/adberilgen/springsecurityproject/dto/CreateUserRequest.java
+++ src/main/java/com/adberilgen/springsecurityproject/dto/CreateUserRequest.java
@@ -1,14 +1,16 @@
 package com.adberilgen.springsecurityproject.dto;
 
 import com.adberilgen.springsecurityproject.model.Role;
+import lombok.Builder;
 
 import java.util.Set;
 
+@Builder
 public record CreateUserRequest(
-    String name,
-    String username,
-    String  password,
-    Set<Role> authorities
-){
+        String name,
+        String username,
+        String password,
+        Set<Role> authorities
+) {
 
 }
